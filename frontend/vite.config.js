c1a76cb342d2c572f3efb8d9bc8ba20682536d2e
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2026-05-05 18:49:23 -0300

    ainda n funciona

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true, // Adicione isso para melhorar a resolução no Windows
+    strictPort: true, 
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000', // Use o IP direto para evitar problemas de DNS interno
         changeOrigin: true,
+        secure: false,
       }
     }
   }
